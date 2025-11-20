e0306306ea68b7d34b1491938618ba44a2a7d69c
D-Faris-UCL <zcabdfb@ucl.ac.uk>
2025-11-20 20:44:27 +0000

    U

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ a = time.localtime().tm_sec
 b = time.localtime().tm_min
 c = a+b
 
-print(a)
-print(b)
+print(a,flush=True)
+print(b,flush=True)
 
-for i in range(10):
+for i in range(100):
     c = a+b
     a = b
     b = c
-    print(c)
+    print(c,flush=True)
