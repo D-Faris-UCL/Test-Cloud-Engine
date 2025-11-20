5908c9a62e929083a001aeddcfd1ca65e7c4d043
D-Faris-UCL <zcabdfb@ucl.ac.uk>
2025-11-20 20:40:52 +0000

    U

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+import time
+
+a = time.localtime().tm_sec
+b = time.localtime().tm_min
+c = a+b
+
+print(a)
+print(b)
+
+for i in range(10):
+    c = a+b
+    a = b
+    b = c
+    print(c)
